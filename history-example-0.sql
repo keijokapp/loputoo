c503fbbe21824c8324baef9df1698c71ac9704ad
keijokapp <keijo.kapp@gmail.com>
2016-02-01 07:55:12 +0200

    ...

---
+++ history-example-0.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS history_test;
+CREATE table history_test(
+	id BIGSERIAL PRIMARY KEY,
+	previous_revision BIGINT REFERENCES history_test(id) UNIQUE
+)
+
+INSERT INTO history_test(previous_revision) VALUES(0);
+SELECT * FROM history_test;
---
+++ history-example-1.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS history_test;
+CREATE table history_test(
+	id BIGSERIAL PRIMARY KEY,
+	object_uuid VARCHAR(36)
+)
+
+INSERT INTO history_test(object_uuid) VALUES('');
+SELECT * FROM history_test;
